d8e1444f7b40f437291b9e66f1b2b1d47598bf86
JiHeeeSeo <jiheeseo@kangwon.ac.kr>
2022-06-09 01:05:56 +0900

    🤜 solving this problem

--- baekjoon/10_재귀/2447/2447/main.cpp
+++ baekjoon/10_재귀/2447/2447/main.cpp
@@ -4,43 +4,40 @@
 using namespace std;
 
 
-void recu(int n);
+void recu(int i, int j, int n);
 
 int main() {
 	int n;
 	cin >> n;
 
-	recu(n);
-}
-
-
-
-
-void recu(int n) {
-	int div = n / 3;
-
-	if (n == 3) {
-		for (int i = 1; i <= n; i++) {
-			for (int k = 1; k <= n; k++) {
-				if (i == 2 && k == 2) {
-					printf(" ");
-					continue;
-				}
-				printf("*");
-			}
-			printf("\n");
-		}
-		return;
-	}
-
-	for (int i = 1; i <= n; div++) {
-		for (int k = 1; k <= n; div++) {
-			recu(div);
-			if (i == 1 + div && k == 1 + div)
-				continue;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			recu(i, j, n);
 		}
 		printf("\n");
 	}
+	
+}
+
+
+void recu(int i, int k, int n) {
+	int div = n / 3;
+
+	if (div == 3) {
+
+		if (i%3 == 2 && k%3 == 2) {
+			printf(" ");
+		}
+		else
+			printf("*");
+		return;
+	}
+	
+	if (i == (div * 4 + 1) && k==(div*4+1)) {
+		
+	}
+	else
+		recu(i, k, div);
 
 }
 
