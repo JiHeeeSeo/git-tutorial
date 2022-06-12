7e93ef5bbf1da35a447fb21ee3e275977641f175
JiHeeeSeo <jiheeseo@kangwon.ac.kr>
2022-06-13 02:51:40 +0900

    💬solving 11729

--- baekjoon/10_재귀/11729/11729/main.cpp
+++ baekjoon/10_재귀/11729/11729/main.cpp
@@ -2,7 +2,21 @@
 #include <iostream>
 
 using namespace std;
+void recu(int n, int start, int end);
 
 int main() {
+	int n;
+	cin >> n;
+
+
+}
+
+void recu(int n, int start, int end) {
+	if (n == 1) {
+		printf("%d %d\n", start, end);
+	}
+	recu(n - 1, start, end);
+
+
 
 }
