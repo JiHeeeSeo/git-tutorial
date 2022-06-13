8a5cf4bbdbc9fcaf7ce38564433cde41e5c14504
JiHeeeSeo <jiheeseo@kangwon.ac.kr>
2022-06-14 01:20:33 +0900

    ✌ solved 11729

--- baekjoon/10_재귀/11729/11729/main.cpp
+++ baekjoon/10_재귀/11729/11729/main.cpp
@@ -1,22 +1,26 @@
 #include <cstdio>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-void recu(int n, int start, int end);
+void recu(int n, int start, int by, int end);
 
 int main() {
 	int n;
 	cin >> n;
 
+	printf("%d\n", (int)pow(2.0, n) - 1);
+	recu(n, 1, 2, 3);
 
 }
 
-void recu(int n, int start, int end) {
+void recu(int n, int start, int by, int end) {
 	if (n == 1) {
 		printf("%d %d\n", start, end);
+		return;
 	}
-	recu(n - 1, start, end);
-
-
+	recu(n - 1, start, end, by);
+	printf("%d %d\n", start, end);
+	recu(n - 1, by, start, end);
 
 }
