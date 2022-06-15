d86593ac6679aac2c812a902352408ac851cd313
JiHeeeSeo <jiheeseo@kangwon.ac.kr>
2022-06-16 00:52:07 +0900

    ✌ solved 2231

--- baekjoon/11_브루트 포스/2231/main.cpp
+++ baekjoon/11_브루트 포스/2231/main.cpp
@@ -1,6 +1,25 @@
 #include <cstdio>
 #include <iostream>
 
+using namespace std;
+
 int main() {
+	int n, sum, k;
+	cin >> n;
+
+	int result = 0;
+	
+	for (int i = (n-1); i > 0; i--) {
+		sum = i;
+		k = i;
+		while (k > 0) {
+			sum += k % 10;
+			k /= 10;
+		}
+		if (sum == n) {
+			result = i;
+		}
+	}
+	printf("%d", result);
 
 }
